25eabc201dcd7f84f02a18418efcdab5f322fee4
Morales18 <lpejor8@gmail.com>
2023-02-27 21:26:22 +0100

    vistas config

---
+++ app/src/main/java/es/TFG/MiHipotecaApp/UsuarioRegistrado/InfoPerfilUsuario.java
@@ -0,0 +1,16 @@
+package es.TFG.MiHipotecaApp.UsuarioRegistrado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import es.TFG.MiHipotecaApp.R;
+
+public class InfoPerfilUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_perfil_usuario);
+    }
+}
--- app/src/main/java/es/TFG/MiHipotecaApp/UsuarioRegistrado/PaginaPrincipal.java
+++ app/src/main/java/es/TFG/MiHipotecaApp/UsuarioRegistrado/PaginaPrincipal.java
@@ -2,15 +2,38 @@ package es.TFG.MiHipotecaApp.UsuarioRegistrado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import es.TFG.MiHipotecaApp.R;
 
 public class PaginaPrincipal extends AppCompatActivity {
+    private Button btn_simular_hipoteca;
+    private Button btn_config_usuario;
+    private CircleImageView foto_perfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagina_principal);
+        btn_simular_hipoteca = findViewById(R.id.btn_simular_hipoteca_pag_principal);
+        foto_perfil = findViewById(R.id.foto_perfil_pag_principal);
+        btn_config_usuario = findViewById(R.id.btn_config_usuario);
+        eventos();
     }
+
+    public void eventos(){
+        btn_config_usuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(PaginaPrincipal.this, InfoPerfilUsuario.class);
+                startActivity(i);
+            }
+        });
+    }
+
+
 }
