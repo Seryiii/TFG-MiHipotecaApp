bde53d25355e20efa1f930e8746cf06d0eea8f26
carlossr15 <csobrado@ucm.es>
2023-03-01 19:24:27 +0100

    AlertDialog

--- MiHipotecaApp/app/src/main/java/es/MiHipotecaApp/TFG/UsuarioRegistrado/InfoPerfilUsuario.java
+++ MiHipotecaApp/app/src/main/java/es/MiHipotecaApp/TFG/UsuarioRegistrado/InfoPerfilUsuario.java
@@ -1,6 +1,15 @@
 package es.MiHipotecaApp.TFG.UsuarioRegistrado;
 
+import static android.app.PendingIntent.getActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +17,36 @@ import es.MiHipotecaApp.TFG.R;
 
 public class InfoPerfilUsuario extends AppCompatActivity {
 
+    private Button eliminar_cuenta;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_perfil_usuario);
+        initUI();
+    }
+
+    private void initUI() {
+        eliminar_cuenta = findViewById(R.id.eliminar_cuenta);
+        eliminar_cuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog dialogo = new AlertDialog.Builder(InfoPerfilUsuario.this)
+                        .setPositiveButton(getString(R.string.si_eliminar_cuenta), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setNegativeButton(getString(R.string.no_eliminar_cuenta), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setTitle("ELIMINAR CUENTA").setMessage("¿Desea eliminar la cuenta?").create();
+                dialogo.show();
+            }
+        });
     }
 }
