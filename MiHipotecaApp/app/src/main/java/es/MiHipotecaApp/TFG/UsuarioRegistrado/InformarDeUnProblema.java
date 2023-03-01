f3f83c0b7304d82e5aa3a21603a16ce98c94ddba
carlossr15 <csobrado@ucm.es>
2023-03-01 20:15:20 +0100

    Vista notificar problema (front-end)

---
+++ MiHipotecaApp/app/src/main/java/es/MiHipotecaApp/TFG/UsuarioRegistrado/InformarDeUnProblema.java
@@ -0,0 +1,16 @@
+package es.MiHipotecaApp.TFG.UsuarioRegistrado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import es.MiHipotecaApp.TFG.R;
+
+public class InformarDeUnProblema extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informar_de_un_problema);
+    }
+}
