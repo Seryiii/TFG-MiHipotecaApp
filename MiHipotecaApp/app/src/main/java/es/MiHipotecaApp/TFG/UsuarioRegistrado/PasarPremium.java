4973b458a00ad03e68cebfc9a2cdea70a4435348
Morales18 <lpejor8@gmail.com>
2023-03-03 13:28:30 +0100

    vista pasar premium a medias

---
+++ MiHipotecaApp/app/src/main/java/es/MiHipotecaApp/TFG/UsuarioRegistrado/PasarPremium.java
@@ -0,0 +1,27 @@
+package es.MiHipotecaApp.TFG.UsuarioRegistrado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ViewFlipper;
+
+import es.MiHipotecaApp.TFG.R;
+
+public class PasarPremium extends AppCompatActivity {
+    private ViewFlipper vf_1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pasar_premium);
+        vf_1 = findViewById(R.id.view_flipper);
+        vf_1.setFlipInterval(3000);
+        vf_1.startFlipping();
+    }
+    public void previousView(View v){
+        vf_1.showPrevious();
+    }
+    public void nextView(View v){
+        vf_1.showNext();
+    }
+}
