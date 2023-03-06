b7e774f0b24e4049778f7404226b6bd855675955
Morales18 <lpejor8@gmail.com>
2023-03-06 12:13:14 +0100

    filtros con valor debajo

--- MiHipotecaApp/app/src/main/java/es/MiHipotecaApp/TFG/Filtros.java
+++ MiHipotecaApp/app/src/main/java/es/MiHipotecaApp/TFG/Filtros.java
@@ -4,17 +4,61 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Filtros extends AppCompatActivity {
     private Spinner sp_bancos;
+    private SeekBar precio , primer_pago, plazo;
+    private TextView tv_precio , tv_primer_pago, tv_plazo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtros);
         sp_bancos = findViewById(R.id.sp_bancos);
+        precio = findViewById(R.id.seekBar_precio);
+        primer_pago = findViewById(R.id.seekBar_primer_pago);
+        plazo = findViewById(R.id.seekBar_plazo_prestamo);
+        tv_precio = findViewById(R.id.tv_precio);
+        tv_primer_pago = findViewById(R.id.tv_primer_pago);
+        tv_plazo = findViewById(R.id.tv_plazo_prestamo);
         String [] bancos = {"ING","SANTANDER","BBVA","CAIXABANK","BANKINTER","EVO BANCO","SABADELL","UNICAJA","DEUTSCHE BANK","OPEN BANK","KUTXA BANK","IBERCAJA","ABANCA"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,bancos);
         sp_bancos.setAdapter(adapter);
+        eventos();
+    }
+
+    private void eventos() {
+        precio.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tv_precio.setText(String.valueOf(i));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+        primer_pago.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tv_primer_pago.setText(String.valueOf(i));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+        plazo.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tv_plazo.setText(String.valueOf(i));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
     }
 }
