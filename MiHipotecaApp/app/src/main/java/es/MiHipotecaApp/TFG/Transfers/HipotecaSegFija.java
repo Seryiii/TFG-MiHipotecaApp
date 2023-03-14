086b637378eafde40045fcad57fcd55929eb8282
carlossr15 <csobrado@ucm.es>
2023-03-14 19:17:47 +0100

    Grafico aportado vs restante + otros cambios relacionados con graficos

--- MiHipotecaApp/app/src/main/java/es/MiHipotecaApp/TFG/Transfers/HipotecaSegFija.java
+++ MiHipotecaApp/app/src/main/java/es/MiHipotecaApp/TFG/Transfers/HipotecaSegFija.java
@@ -10,14 +10,55 @@ public class HipotecaSegFija extends HipotecaSeguimiento implements Serializable
     public HipotecaSegFija(String nombre, String comunidad_autonoma, String tipo_vivienda, String antiguedad_vivienda, double precio_vivienda, double cantidad_abonada, int plazo_anios, int anio_hipoteca_actual, int mes_hipoteca_actual, double totalGastos, double totalVinculacionesAnual, String banco_asociado, double porcentaje_fijo) {
         super(nombre, comunidad_autonoma, tipo_vivienda, antiguedad_vivienda, precio_vivienda, cantidad_abonada, plazo_anios, anio_hipoteca_actual, mes_hipoteca_actual, totalGastos, totalVinculacionesAnual, banco_asociado);
         this.porcentaje_fijo = porcentaje_fijo;
+        obtenerCuotaMensual();
     }
+
+    private double cuotaMensual;
 
     public double getPorcentaje_fijo() {
         return porcentaje_fijo;
+    }
+
+    public double getCuotaMensual() {
+        return cuotaMensual;
     }
 
     public void setPorcentaje_fijo(double porcentaje_fijo) {
         this.porcentaje_fijo = porcentaje_fijo;
     }
 
+    public void obtenerCuotaMensual(){
+        double aux = Math.pow((1 + (porcentaje_fijo / 100) / 12), plazo_anios * 12);
+        cuotaMensual = ((precio_vivienda - cantidad_abonada) * ((porcentaje_fijo / 100) / 12))/(1 -(1 / aux));
+    }
+
+    //Funcion que devuelve el interes pagado en funcion del capital que quede por pagar
+    public double calcularInteresMensualConCapitalPendiente(double capitalPendiente){
+        return capitalPendiente * (porcentaje_fijo / 100) / 12;
+    }
+
+    //Funcion que devuelve el interes pagado de un determinado numero de pago
+    public double calcularInteresMensualConNumeroMes(int numPago){
+        double capitalPendiente = cuotaMensual * numPago;
+        return capitalPendiente * (porcentaje_fijo / 100) / 12;
+    }
+
+    //Llamar a esta funcion para actualizar el capital pendiente --> capPendiente -= h.obtenerobtenerCapitalAmortizadoMensual(capPendiente)
+    public double obtenerCapitalAmortizadoMensual(double capitalPendiente){
+        return cuotaMensual - calcularInteresMensualConCapitalPendiente(capitalPendiente);
+    }
+
+    private double interesesTotales = 0;
+    public void obtenerImpuestosTotales(){
+        for(int i = 0; i < plazo_anios * 12; i++){
+            interesesTotales += calcularInteresMensualConNumeroMes(i);
+        }
+    }
+
+    public double obtenerDineroAportadoActual(int numPago){
+        return cuotaMensual*numPago;
+    }
+    public double obtenerDineroRestanteActual(int numPago){
+        return cuotaMensual*(plazo_anios*12 - numPago);
+    }
 }
--- MiHipotecaApp/app/src/main/java/es/MiHipotecaApp/TFG/UsuarioRegistrado/HipotecasSeguimiento/VisualizarHipotecaSeguimiento.java
+++ MiHipotecaApp/app/src/main/java/es/MiHipotecaApp/TFG/UsuarioRegistrado/HipotecasSeguimiento/VisualizarHipotecaSeguimiento.java
@@ -1,13 +1,25 @@
 package es.MiHipotecaApp.TFG.UsuarioRegistrado.HipotecasSeguimiento;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import es.MiHipotecaApp.TFG.R;
+import es.MiHipotecaApp.TFG.Transfers.HipotecaSegFija;
 import es.MiHipotecaApp.TFG.Transfers.HipotecaSeguimiento;
 import es.MiHipotecaApp.TFG.UsuarioRegistrado.HipotecasSeguimiento.Graficos.BarChartActivity;
 import es.MiHipotecaApp.TFG.UsuarioRegistrado.HipotecasSeguimiento.Graficos.GraficosHipotecaFija;
@@ -19,17 +31,64 @@ public class VisualizarHipotecaSeguimiento extends AppCompatActivity {
     private Button goPieChart;
     private Button goRadarChart;
 
-    private HipotecaSeguimiento hip;
+    private PieChart aportado_vs_a_financiar;
+    private PieChart gastos_totales;
+    private LineChart intereses_vs_capital_mensual;
+
+    private HipotecaSegFija hip;
+    private Button btn_aportado_vs_financiar_valor;
+    private Button btn_aportado_vs_financiar_porcentaje;
+    private Button btn_gastos_totales_valor;
+
+    private Button btn_gastos_totales_porcentaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualizar_hipoteca_seguimiento);
 
-        hip = (HipotecaSeguimiento) getIntent().getSerializableExtra("hipoteca");
+        hip = (HipotecaSegFija) getIntent().getSerializableExtra("hipoteca");
         goBarChart = findViewById(R.id.go_bar_chart);
         goPieChart = findViewById(R.id.go_pie_chart);
         goRadarChart = findViewById(R.id.go_radar_chart);
+
+        btn_aportado_vs_financiar_valor = findViewById(R.id.btn_valor_aportado_vs_a_financiar);
+        btn_aportado_vs_financiar_porcentaje = findViewById(R.id.btn_porcentaje_aportado_vs_a_financiar);
+        aportado_vs_a_financiar = findViewById(R.id.pie_chart_aportado_vs_a_financiar);
+
+        btn_gastos_totales_valor = findViewById(R.id.btn_valor_gastos_totales);
+        btn_gastos_totales_porcentaje = findViewById(R.id.btn_porcentaje_gastos_totales);
+
+        //Hipoteca de prueba, coger la de la base de datos
+        hip = new HipotecaSegFija("h", "m", "", "", 100000, 50000, 25, 4, 4, 0, 0, "", 9);
+
+        pieChartAportadoVsFinanciarValor(); //Se visualiza por defecto el grafico de valor
+        btn_aportado_vs_financiar_valor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pieChartAportadoVsFinanciarValor();
+            }
+        });
+        btn_aportado_vs_financiar_porcentaje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pieChartAportadoVsFinanciarPorcentaje();
+            }
+        });
+
+        //pieChartGastosTotalesValor(); //Se visualiza por defecto el grafico de valor
+        btn_gastos_totales_valor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //pieChartGastosTotalesValor();
+            }
+        });
+        btn_gastos_totales_porcentaje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //pieChartGastosTotalesPorcentaje();
+            }
+        });
 
         goBarChart.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,5 +102,78 @@ public class VisualizarHipotecaSeguimiento extends AppCompatActivity {
 
         goRadarChart.setOnClickListener(v -> startActivity(new Intent(this, RadarChartActivity.class)));
     }
+
+    public void pieChartAportadoVsFinanciarValor(){
+        {
+
+            ArrayList<PieEntry> list = new ArrayList();
+
+            list.add(new PieEntry((float) hip.obtenerDineroAportadoActual(hip.getAnio_hipoteca_actual()*12 + hip.getMes_hipoteca_actual()), "DINERO APORTADO"));
+            list.add(new PieEntry((float) hip.obtenerDineroRestanteActual(hip.getAnio_hipoteca_actual()*12 + hip.getMes_hipoteca_actual()), "DINERO RESTANTE"));
+
+
+            PieDataSet pieDataSet = new PieDataSet(list, "List");
+
+            List<Integer> colors = new ArrayList<>();
+            colors.add(Color.RED);
+            colors.add(Color.BLUE);
+            colors.add(Color.GREEN);
+            colors.add(Color.YELLOW);
+            pieDataSet.setColors(colors);
+
+            pieDataSet.setValueTextColor(Color.BLACK);
+            pieDataSet.setValueLineColor(Color.BLACK);
+            pieDataSet.setValueTextSize(25);
+
+            PieData pieData = new PieData(pieDataSet);
+
+            aportado_vs_a_financiar.setData(pieData);
+            Description d = new Description();
+            d.setText("Pie Chart");
+            aportado_vs_a_financiar.setDescription(d);
+            aportado_vs_a_financiar.animateY(2000);
+        }
+    }
+
+    public void pieChartAportadoVsFinanciarPorcentaje(){
+        {
+
+            ArrayList<PieEntry> list = new ArrayList();
+            double auxAportado = hip.obtenerDineroAportadoActual(hip.getAnio_hipoteca_actual()*12 + hip.getMes_hipoteca_actual());
+            double auxRestante = hip.obtenerDineroRestanteActual(hip.getAnio_hipoteca_actual()*12 + hip.getMes_hipoteca_actual());
+            double auxTotal = auxAportado + auxRestante;
+            auxAportado = pasarEnteroAPorcentaje(auxAportado, auxTotal);
+            auxRestante = pasarEnteroAPorcentaje(auxRestante, auxTotal);
+            list.add(new PieEntry((float) auxAportado, "DINERO APORTADO"));
+            list.add(new PieEntry((float) auxRestante, "DINERO RESTANTE"));
+
+
+            PieDataSet pieDataSet = new PieDataSet(list, "List");
+
+            List<Integer> colors = new ArrayList<>();
+            colors.add(Color.RED);
+            colors.add(Color.BLUE);
+            colors.add(Color.GREEN);
+            colors.add(Color.YELLOW);
+            pieDataSet.setColors(colors);
+
+            pieDataSet.setValueTextColor(Color.BLACK);
+            pieDataSet.setValueLineColor(Color.BLACK);
+            pieDataSet.setValueTextSize(25);
+
+            PieData pieData = new PieData(pieDataSet);
+
+            aportado_vs_a_financiar.setData(pieData);
+            Description d = new Description();
+            d.setText("Pie Chart");
+            aportado_vs_a_financiar.setDescription(d);
+            aportado_vs_a_financiar.animateY(2000);
+        }
+    }
+
+    public double pasarEnteroAPorcentaje(double entero, double total){
+        return (entero * 100) / total;
+    }
+
 }
 
