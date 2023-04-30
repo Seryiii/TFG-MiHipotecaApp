9da33dc1c6e059caaee2882a570fa2bd9978ac20
Seryiii <62355786+Seryiii@users.noreply.github.com>
2023-04-30 17:06:48 +0200

    Funciona ya cargar bd de euribor correctamente y peticiones a servidor

--- MiHipotecaApp/app/src/main/java/es/MiHipotecaApp/TFG/EuriborHistorico.java
+++ MiHipotecaApp/app/src/main/java/es/MiHipotecaApp/TFG/EuriborHistorico.java
@@ -2,6 +2,8 @@ package es.MiHipotecaApp.TFG;
 
 import android.content.Context;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -14,11 +16,13 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -45,7 +49,15 @@ public class EuriborHistorico extends AppCompatActivity {
         requestQueue = Volley.newRequestQueue( this.context,new HurlStack());
     }
     protected void ActualizarEuribor() {
+        //147.96.81.245:5000
+        //http://10.0.2.2:5000
         String url = "http://147.96.81.245:5000/EuriborHistorico";
+        System.out.println("Intenado obtener Euribor");
+        Log.w(TAG,"Error al intentar acceder a Euribor : ");
+
+        // Request a string response from the provided URL.
+        final boolean[] euriborObtenido = {false};
+
         JsonObjectRequest request = new JsonObjectRequest(
                 Request.Method.GET,
                 url,
@@ -54,9 +66,11 @@ public class EuriborHistorico extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
 
+                        System.out.println("Intenado obtener Euribor onResponse");
                         JSONArray mJsonArray = null;
+
                         try {
-                            mJsonArray = response.getJSONArray("prueba");
+                            mJsonArray = response.getJSONArray("Eur_dos_ult_meses");
                         } catch (JSONException e) {
                             throw new RuntimeException(e);
                         }
@@ -69,40 +83,72 @@ public class EuriborHistorico extends AppCompatActivity {
                                 String valor_euribor = jsonObject.getString("valor");
                                 Double valor=Double.parseDouble(valor_euribor);
                                 Euribor eu=new Euribor(anio,mes,valor);
-                                db.collection("euribor").add(eu).addOnSuccessListener(new OnSuccessListener<DocumentReference>(){
+                                db.collection("euribor")
+                                        .whereEqualTo("anio", anio)
+                                        .whereEqualTo("mes", mes)
+                                        .get()
+                                        .addOnCompleteListener(task -> {
+                                            if (task.isSuccessful()) {
+                                                QuerySnapshot querySnapshot = task.getResult();
+                                                if (querySnapshot != null && !querySnapshot.isEmpty()) {
+                                                    // El documento ya existe en la base de datos
+                                                    Log.d(TAG, "El documento ya existe en la base de datos");
+                                                } else {
+                                                    // El documento no existe en la base de datos, agrégalo
+                                                    db.collection("euribor")
+                                                            .add(eu)
+                                                            .addOnSuccessListener(documentReference -> {
+                                                                Log.d(TAG, "euribor registrado con exito en Firestore");
+                                                            })
+                                                            .addOnFailureListener(e -> {
+                                                                Log.w(TAG, "Error al registrar euribor en Firestore", e);
+                                                            });
+                                                }
+                                            } else {
+                                                Log.w(TAG, "Error al consultar euribor en Firestore", task.getException());
+                                            }
+                                        });
+                                /*db.collection("euribor").add(eu).addOnSuccessListener(new OnSuccessListener<DocumentReference>(){
 
 
                                     @Override
-                                    public void onSuccess(DocumentReference documentReference) {
-                                        Log.w(TAG,"euribor registrado con exito en Firestore: ");
-                                        finish();
-                                    }
+                                public void onSuccess(DocumentReference documentReference) {
+                                        if (euriborObtenido[0]) {
+                                            return;
+                                        }
+
+                                        euriborObtenido[0] = true;
+                                    Log.w(TAG,"euribor registrado con exito en Firestore: ");
+                                    finish();
+                                }
                                 }).addOnFailureListener(new OnFailureListener() {
                                     @Override
                                     public void onFailure(@NonNull Exception e) {
                                         Log.w(TAG,"Error al registrar euribor en Firestore: ");
                                     }
-                                });
+                                });*/
 
                             } catch (JSONException e) {
                                 throw new RuntimeException(e);
                             }
 
                         }
+                        finish();
                     }
                 }, new Response.ErrorListener() {
-            public void onErrorResponse(VolleyError error) {
+                public void onErrorResponse(VolleyError error) {
 
 
-                Log.d("PETICIONES", error.toString());
-            }
+                    Log.d("PETICIONES", error.toString());
+                }
         }
         );
         request.setRetryPolicy(new DefaultRetryPolicy(
                 60000, // segundos
                 0, // 1 reintentos
                 DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-        //requestQueue.add(request);
+        requestQueue.add(request);
+
     }
 }
 
